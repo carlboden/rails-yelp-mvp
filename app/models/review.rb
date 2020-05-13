15f94da3cbed2f2be7242347c6b45537db211f2c
Carl Boden <carl_boden@hotmail.com>
2020-05-13 14:05:18 +0200

    controllers

---
+++ app/models/review.rb
@@ -0,0 +1,10 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :restaurant, presence: true
+  validates :content, :rating, presence: true
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+end
+
+
+
